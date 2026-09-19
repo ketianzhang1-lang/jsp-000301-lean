2e12771b97ec8a27171d3a3a8dff16f8e6f921d3
ketianzhang1-lang <ketianzhang1@gmail.com>
2026-09-18 22:18:50 -0400

    Develop JSP-000897 supplementary theorem and verification: AuditSupplement.lean

---
+++ projects/jsp-000897/AuditSupplement.lean
@@ -0,0 +1,17 @@
+import JSP000897Stability
+
+#print axioms Erdos1079.turan_split_le
+#print axioms Erdos1079.maximumDegree_edge_bound
+#print axioms Erdos1079.erdos_problem_1079
+#print axioms Erdos1079.erdos_1079
+#print axioms JSP000897.surplus_at_every_maximum
+#print axioms JSP000897.resolution_with_surplus
+#print axioms JSP000897.surplus_dominates
+#print axioms JSP000897.strict_resolution
+#print axioms JSP000897.edge_bound_by_maxDegree
+#print axioms JSP000897.cliqueExtremal_split_from_three
+#print axioms JSP000897.surplus_with_degree_deficit
+#print axioms JSP000897.surplus_loss_bound
+#print axioms JSP000897.strict_link_with_degree_deficit
+#print axioms JSP000897.surplus_at_every_maximum_from_three
+#print axioms JSP000897.complete_with_stability
