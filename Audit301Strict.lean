2f79345fd0be14f279849bdeae183d4b5791a619
ketianzhang1-lang <ketianzhang1@gmail.com>
2026-09-18 21:43:12 -0400

    Add strict JSP-000301 target verification: Audit301Strict.lean

---
+++ Audit301Strict.lean
@@ -0,0 +1,10 @@
+import JSP000301
+
+#print axioms JSP000301.Powerful
+#print axioms JSP000301.PerfectSquare
+#print axioms JSP000301.powerful_12167
+#print axioms JSP000301.powerful_12168
+#print axioms JSP000301.not_square_12167
+#print axioms JSP000301.not_square_12168
+#print axioms JSP000301.jsp_000301_counterexample
+#print axioms JSP000301.jsp_000301_disproved
